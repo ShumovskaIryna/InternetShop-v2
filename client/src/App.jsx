e6c270cbadffa3d2323c27bb3c47a017ed942adb
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-09-20 13:01:21 +0300

    route & Link to Cart component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import { Routes, Route } from 'react-router-dom';
 import Products from './components/Products';
+import Cart from './components/Cart';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/products" element={<Products />} />
+        <Route exact path="/cart" element={<Cart />} />
       </Routes>
     </div>
   );
