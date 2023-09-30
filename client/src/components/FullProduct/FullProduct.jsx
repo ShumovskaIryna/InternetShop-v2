6e59b6765ebc7d909c0073d7d2fa97faa01db4ac
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-10-01 01:27:17 +0300

    adaptive design for FullProduct (Gallery)

--- client/src/components/FullProduct/FullProduct.jsx
+++ client/src/components/FullProduct/FullProduct.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 import MiniGallery from './MiniGallery';
-import SomeErrorScreen from '../SomeErrorScreen'
+import SomeErrorScreen from '../SomeErrorScreen';
 import ValueAttributes from './ValueAttributes';
 import { QUERIES } from '../../constants/costants';
 
@@ -11,63 +11,74 @@ const FullProduct = (props) => {
     data: dataProduct,
     loading: loadingProduct,
   } = useQuery(QUERIES.GET_PRODUCT_BY_ID, {
-    variables: {id: props.productId}
+    variables: { id: props.productId },
   });
 
   return (
-    <> 
-    {errorProduct ? <SomeErrorScreen toggleProductDetails={props.toggleProductDetails}/> : 
     <>
-    {loadingProduct ? <div> Loading...</div>
-    :
-      <div className="full-item">
-        {' '}
-        <div className="full-product-card">
-          {' '}
-          <div className="close" 
-              onClick={() => {
-                props.toggleProductDetails(current => !current)
-              }}>
-            Close
-          </div>
-          <MiniGallery photos={dataProduct.product.gallery} />
-          <img src={dataProduct.product.gallery[0]} className="full-gallery" alt="product" />
-          <div className="full-descrtiption">
-            <p className="brand">{dataProduct.product.brand}</p>
-            <p className="name">{dataProduct.product.name}</p>
-            <div className="description">
-              <div className="all-attributs">
-                {dataProduct.product?.attributes.map((el, id) => (
-                  <div className="attribut" key={id}>
-                    {el.name}:
-                    <ValueAttributes atributes={el.items} />
+      {errorProduct ? (
+        <SomeErrorScreen toggleProductDetails={props.toggleProductDetails} />
+      ) : (
+        <>
+          {loadingProduct ? (
+            <></>
+          ) : (
+            <div className="black">
+              {' '}
+              <div className="white">
+                {' '}
+                <div
+                  className="close"
+                  onClick={() => {
+                    props.toggleProductDetails((current) => !current);
+                  }}
+                >
+                  Close
+                </div>
+                <div className="full-container">
+                  <div className="full-gallery">
+                    <MiniGallery photos={dataProduct.product.gallery} />
+                    <img src={dataProduct.product.gallery[0]} className="image" alt="product" />
                   </div>
-                ))}
+                  <div className="full-descrtiption">
+                    <p className="brand">{dataProduct.product.brand}</p>
+                    <p className="name">{dataProduct.product.name}</p>
+                    <div className="description">
+                      <div className="all-attributs">
+                        {dataProduct.product?.attributes.map((el, id) => (
+                          <div key={id}>
+                            <p className="attribut">{el.name}:</p>
+                            <ValueAttributes atributes={el.items} />
+                          </div>
+                        ))}
+                      </div>
+                      <b className="price">
+                        <p className="attribut">Price:</p>
+                        {dataProduct.product.prices[0].amount}
+                        {dataProduct.product.prices[0].currency.symbol}
+                      </b>
+                      <div
+                        className="full_add_button"
+                        onClick={() => props.addProduct(dataProduct.product)}
+                        title="Add to cart"
+                      >
+                        <label className="add-label">Add to cart</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="full-container">
+                  <p
+                    className="content"
+                    dangerouslySetInnerHTML={{ __html: dataProduct.product.description }}
+                  ></p>
+                </div>
               </div>
-              <b className="price">
-                <p className="atribut">Price:</p>
-                {dataProduct.product.prices[0].amount}
-                {dataProduct.product.prices[0].currency.symbol}
-              </b>
-              <div
-                className="full-add-to-cart"
-                onClick={() => props.addProduct(dataProduct.product)}
-                title="Add to cart"
-              >
-                <label className="cart-label">Add to cart</label>
-              </div>
-              <p
-                className="content"
-                dangerouslySetInnerHTML={{ __html: dataProduct.product.description }}
-              ></p>
             </div>
-          </div>
-        </div>
-      </div>
-    }
+          )}
+        </>
+      )}
     </>
-  }
-    </>
-  )
-}
+  );
+};
 export default FullProduct;
--- client/src/components/FullProduct/Photo.js
+++ client/src/components/FullProduct/Photo.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Photo extends Component {
   render() {
-    return <img src={this.props.src} className="full-gallery1"></img>;
+    return <img src={this.props.src} className="mini-image"></img>;
   }
 }
 
