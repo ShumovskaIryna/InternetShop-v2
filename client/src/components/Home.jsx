64e904330d93a74d44d7c2544989cf0b2b724443
Iryna Shumovska <shumovska_ia@groupbwt.com>
2023-10-01 21:16:29 +0300

    adaptive design for Home & categories

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="hero">
       <div className="card text-bg-dark">
-        <img src="/assets/red-panda.jpeg" className="card-img" alt="Red_Panda" height="460px" />
+        <img src="/assets/red-panda.jpeg" className="card-img" alt="Red_Panda" />
         <div className="card-img-overlay d-flex flex-column justify-content-center">
           <div className="container">
             <h5 className="card-title display-3 fw-bolder mb-0">LET'S GO SHOPPING</h5>
